69e4557ad8df643aa48eab80ce3f06801fc3187b
hmujica@distritomoda.com.ar <hmujica@distritomoda.com.ar>
2019-01-22 23:00:32 -0300

    include

--- ng-include/js/app.js
+++ ng-include/js/app.js
@@ -8,7 +8,7 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
   
 	$scope.profesores = {};
-
+	$scope.tblProfesores="parciales/tblProfesores.html";
   
   	$http.get('json/profesores.json').success(function(data){
   		//Codigo cuando es correcta la petición
